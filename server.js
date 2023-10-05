156e8a32d819bb6cd42a27269a5db998609b0986
FerLicht <fernando.lichtschein@gmail.com>
2023-10-05 23:32:47 +0000

    parametros en config.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 // import { MongoClient } from "mongodb";
 const { MongoClient } = require('mongodb')
 // Replace the uri string with your MongoDB deployment's connection string.
-const uri = "mongodb://localhost:27017"
+var config   = require('./config');
+var mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
 var date_time = new Date();
 // Create a new client and connect to MongoDB
-const client = new MongoClient(uri);
+const client = new MongoClient(mongoUri);
 
 async function run() {
   try {
     // Connect to the "insertDB" database and access its "haiku" collection
-    const database = client.db("test");
+    const database = client.db(config.mongodb.database);
     const haiku = database.collection("message");
     
     // Create a document to insert
